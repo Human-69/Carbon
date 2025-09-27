0b6bae64a62fc9192c28dab0099838a9c81e6306
MarkCoding <marko.ambrosio.voros@gmail.com>
2025-09-27 11:47:41 +0200

    Moved Sandbox/premake5.lua into premake5.lua

--- Run.bat
+++ Run.bat
@@ -23,23 +23,11 @@ make config=%configuration% || exit \b
 echo "==== Done building Carbon(%configuration%) ===="
 echo.
 
-copy "%~dp0bin\%configuration%\Carbon.lib" "%~dp0Sandbox\lib"
-
-echo "==== Compiling sandbox ===="
-
-cd Sandbox
-premake5 gmake
-if %makeclean%==clean make clean
-make config=%configuration% || exit \b
-
-echo "==== Done building sandbox(%configuration%) ===="
-echo.
-
 echo "==== Running sandbox ===="
 echo.
-cd ../x64/%configuration%
+cd x64/%configuration%
 if %configuration%==debug gdb Sandbox.exe
 if %configuration%==release Sandbox.exe
 echo.
 echo "==== Sandbox has exited with code %errorlevel% ===="
-cd ../../../
+cd ../..
