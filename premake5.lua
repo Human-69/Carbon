6b3b2cc81e1be399e033a24eead54843438c6089
MarkCoding <marko.ambrosio.voros@gmail.com>
2025-09-28 14:51:26 +0200

    Renamed ImGUI to ImGui
    Dependencies/ImGUI has been renamed to Dependencies/ImGui for consistency

--- premake5.lua
+++ premake5.lua
@@ -12,7 +12,7 @@ project "Carbon"
    pchsource "Carbon/src/Core/cbpch.cpp"
 
    files { "Carbon/src/**.h", "Carbon/src/**.cpp", "Carbon/src/**.hpp" }
-   includedirs {"Dependencies/GLFW/include", "Carbon/src", "Dependencies/GLEW/include" , "Dependencies/GLFW/include", "Carbon/src/Core", "vendor/glm/glm", "vendor/include", "vendor", "Dependencies/ImGUI" }
+   includedirs {"Dependencies/GLFW/include", "Carbon/src", "Dependencies/GLEW/include" , "Dependencies/GLFW/include", "Carbon/src/Core", "vendor/glm/glm", "vendor/include", "vendor", "Dependencies/ImGui" }
 
    defines {"CARBON"}
 
@@ -39,8 +39,8 @@ project "Sandbox"
    targetdir "x64/%{cfg.buildcfg}"
 
    files { "Sandbox/src/**.h", "Sandbox/src/**.cpp", "Sandbox/src/**.hpp" }
-   includedirs {"Carbon/src", "Carbon/src/Core", "Sandbox/src", "vendor/glm/glm", "vendor/include", "Dependencies/GLEW/include", "Dependencies/GLFW/include", "vendor",  "Dependencies/ImGUI" }
-   libdirs { "bin/%{cfg.buildcfg}", "Dependencies/GLEW/lib/Release/x64", "Dependencies/ImGUI/bin/ImGui/%{cfg.buildcfg}" }
+   includedirs {"Carbon/src", "Carbon/src/Core", "Sandbox/src", "vendor/glm/glm", "vendor/include", "Dependencies/GLEW/include", "Dependencies/GLFW/include", "vendor",  "Dependencies/ImGui" }
+   libdirs { "bin/%{cfg.buildcfg}", "Dependencies/GLEW/lib/Release/x64", "Dependencies/ImGui/bin/ImGui/%{cfg.buildcfg}" }
    links {"Carbon", "ImGui", "glew32", "glfw3", "opengl32" }
 
    filter { "system:windows", "action:gmake" }
